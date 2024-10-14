d447ec99699de7a781cc9716fb006e7710e2f558
Augustin FERNANDES <augustin.fernandes@student.junia.com>
2024-10-14 17:31:25 +0200

    suppression de fichier

--- tests/fixture.ts
+++
@@ -1,9 +0,0 @@
-import { test as base } from '@playwright/test';
-import { FormLayoutsPage } from './FormLayoutsPage';
-const test = base.extend({
- FormLayouts: async ({ page }, use) => {
- await use(new FormLayoutsPage(page));
- },
-});
-const expect = base.expect;
-export { test, expect };
