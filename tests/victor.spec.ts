4e634a3d7675f2d85d0eff3f4b9c5db415dfb837
Victor HUCKEL <victor.huckel@student.junia.com>
2024-10-14 16:21:01 +0200

    changement victor

--- tests/victor.spec.ts
+++ tests/victor.spec.ts
@@ -1,25 +1,31 @@
 import { test, expect } from '@playwright/test';
+import AmazonHomePage from '../pages/AmazonHomePage';
+import AmazonSearchResultsPage from '../pages/AmazonSearchResultsPage';
+import AmazonProductPage from '../pages/AmazonProductPage';
+import AmazonCheckoutPage from '../pages/AmazonCheckoutPage';
 
 test('Passer une commande avec un login pendant le checkout', async ({ page }) => {
+    const homePage = new AmazonHomePage(page);
+    const searchResultsPage = new AmazonSearchResultsPage(page);
+    const productPage = new AmazonProductPage(page);
+    const checkoutPage = new AmazonCheckoutPage(page);
+
     // Aller sur le site Amazon
     await page.goto('https://www.amazon.fr/');
 
-    // Accepter les cookies (ou toute autre modalité présente)
-    await page.click('#sp-cc-accept');
+    // Accepter les cookies
+    await homePage.acceptCookies();
+
     // Ajouter un produit au panier (rechercher un produit et l'ajouter)
-    await page.fill('input[name="field-keywords"]', 'produit de test');
-    await page.click('input[type="submit"]'); // cliquer sur le bouton "Rechercher"
-    await page.waitForSelector('.s-result-item'); // attendre l'apparition des résultats de recherche
-    await page.click('.s-result-item'); // cliquer sur un produit dans les résultats
-    
-    await page.click('#buy-now-button'); // cliquer sur "Acheter"
-    
+    await homePage.searchForProduct('produit de test');
+    await searchResultsPage.selectFirstProduct();
+    await productPage.clickBuyNow(); // Utiliser la méthode pour cliquer sur "Acheter"
 
     // Se connecter pendant le processus de checkout
-    await page.fill('input[name="email"]', 'votre_email@example.com');
-    await page.click('text=Continuer'); // Clic basé sur le texte
-    await page.fill('input[name="password"]', 'votre_mot_de_passe');
-    await page.click('input[type="submit"]'); // cliquer sur le bouton "Se connecter"
+    await checkoutPage.enterEmail('votre_email@example.com');
+    await checkoutPage.clickContinue(); // Clic basé sur le texte
+    await checkoutPage.enterPassword('votre_mot_de_passe');
+    await checkoutPage.clickLogin(); // Clic sur le bouton "Se connecter"
 
     // Vérifier que la commande est passée avec succès (exemple basique)
     await expect(page).toHaveURL(/.*checkout/); // vérifier que l'utilisateur est sur la page de checkout
