e36b2cc63f722504bc4919a9d7bf2392c06b42f1
Victor HUCKEL <victor.huckel@student.junia.com>
2024-10-21 15:51:20 +0200

    1

--- tests/victor.spec.ts
+++ tests/victor.spec.ts
@@ -5,13 +5,14 @@ import AmazonProductPage from '../pages/AmazonProductPage';
 import AmazonCheckoutPage from '../pages/AmazonCheckoutPage';
 import AmazonPanier from '../pages/AmazonPanier';
 import AmazonCategoryPage from '../pages/AmazonCategoryPage'; // Adjust the path if necessary
+import { AmazonSignInPage } from '../pages/AmazonSignInPage'; // Add this import
 
 test('Passer une commande avec un login pendant le checkout', async ({ page }) => {
     const homePage = new AmazonHomePage(page);
     const searchResultsPage = new AmazonSearchResultsPage(page);
     const productPage = new AmazonProductPage(page);
     const checkoutPage = new AmazonCheckoutPage(page);
-
+    const signInPage = new AmazonSignInPage(page); // Ajouter la page de connexion
     // Aller sur le site Amazon
     await page.goto('https://www.amazon.fr/');
 
@@ -19,18 +20,17 @@ test('Passer une commande avec un login pendant le checkout', async ({ page }) =
     await homePage.acceptCookies();
 
     // Ajouter un produit au panier (rechercher un produit et l'ajouter)
-    await homePage.searchForProduct('produit de test');
+    await homePage.searchForProduct('iphone 16');
     await searchResultsPage.selectFirstProduct();
     await productPage.clickBuyNow(); // Utiliser la méthode pour cliquer sur "Acheter"
 
     // Se connecter pendant le processus de checkout
-    await checkoutPage.enterEmail('votre_email@example.com');
-    await checkoutPage.clickContinue(); // Clic basé sur le texte
-    await checkoutPage.enterPassword('votre_mot_de_passe');
-    await checkoutPage.clickLogin(); // Clic sur le bouton "Se connecter"
+    await signInPage.enterEmail('augustinf59@gmail.com');
+    await signInPage.clickContinue();
+    await signInPage.enterPassword('Bertille1;');
+    await signInPage.clickSignIn();
 
-    // Vérifier que la commande est passée avec succès (exemple basique)
-    await expect(page).toHaveURL(/.*checkout/); // vérifier que l'utilisateur est sur la page de checkout
+
 });
 
 
